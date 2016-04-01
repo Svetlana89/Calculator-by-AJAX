6ecfa7d0746c0975b26269a510034a0b6df01f26
Svetlana <sb.1989@yandex.ru>
2016-04-01 20:13:20 +0300

    Added files via upload

---
+++ ajax.js
@@ -0,0 +1,15 @@
+﻿function start(){
+    var x = document.getElementById("n1").value;
+    var y = document.getElementById("n2").value;
+    
+    var xml = new XMLHttpRequest();
+    xml.onreadystatechange=function(){
+      if (xml.readyState==4 && xml.status==200)
+         document.getElementById('res').innerHTML=xml.responseText;
+   }
+   xml.open('POST','ajax.php',true);
+   xml.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+   var str = 'x='+x+'&y='+y;
+   xml.send(str);
+    
+};
---
+++ ajax.php
@@ -0,0 +1,8 @@
+﻿<?php
+    $a = $_POST["x"];
+    $b = $_POST["y"];
+    $c = $a + $b;
+    if($a<10 && $b<10){
+    echo "Summa = $c";
+    } else echo "Error";
+?>
